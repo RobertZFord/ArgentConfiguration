0cb10395cd312caae03078976dacdf3e64085f82
RobertZFord <RobertZFord@gmail.com>
2019-01-23 21:04:52 -0500

    feat: disable WWAN device

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,7 @@
     };
     udev.extraRules = ''
     SUBSYSTEM=="usb", ATTR{idVendor}=="05c8", ATTR{idProduct}=="036e", ATTR{authorized}="0"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="12d1", ATTR{idProduct}=="1570", ATTR{authorized}="0"
     SUBSYSTEM=="net", ATTR{address}=="02:2c:80:13:92:63", NAME="eth0"
     SUBSYSTEM=="net", ATTR{address}=="9c:d2:1e:60:31:e9", NAME="wlan0"
     '';
